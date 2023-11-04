6c169d3c8f6998d05e6cdc06029c2b70094e1659
tiviin <viniciustivo2@gmail.com>
2023-11-04 12:04:54 -0300

    Tela de login inicial

--- codigo/src/index.js
+++ codigo/src/index.js
@@ -42,3 +42,33 @@ document.getElementById('cadastrarBtn').addEventListener('click', function() {
     alert(`Seja bem-vindo ${nome}. Espero que goste do site!`);
     window.location.href = 'feed.html';
 });
+
+
+document.getElementById('entrarBtn').addEventListener('click', function() {
+    var loginemail = document.getElementById('email').value;
+    var loginsenha = document.getElementById('senha').value;
+
+    var registrosExistentes = localStorage.getItem('cadastros');
+    var registros = [];
+
+    if (registrosExistentes) {
+        registros = JSON.parse(registrosExistentes);
+    }
+
+    var usuarioEncontrado = false;
+    var nomeDoUsuario; 
+
+    registros.forEach(function(registro) {
+        if (registro.email === loginemail && registro.senha === loginsenha) {
+            usuarioEncontrado = true;
+            nomeDoUsuario = registro.nome; 
+        }
+    });
+    if (usuarioEncontrado) {
+        alert(`Seja bem-vindo ${nomeDoUsuario}. Espero que goste do site!`);
+        window.location.href = 'feed.html';
+    } else {
+        alert('Credenciais inválidas. Verifique seu nome de usuário e senha. Caso não possua conta, cadastre-se no botão abaixo do login.');
+    }
+});
+
